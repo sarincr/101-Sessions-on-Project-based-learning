3b9f809fc8ed3d33968a74896c8abf77a1cdd727
Sarin CR <sarin.cr2016@vitstudent.ac.in>
2020-09-18 20:53:16 +0530

    Varying the intensity of light
    Varying the intensity of light according to outside light conditions.

---
+++ 10.AutoIntensityLight/10.AutoIntensityLight.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+int sensor=A0;
+int output=9;
+void setup()
+{
+pinMode(output, OUTPUT);
+}
+void loop()
+{
+int reading=analogRead(sensor);
+int bright=reading/4;
+delay(500);
+analogWrite(output, bright);
+}
---
+++ 10.AutoIntensityLight/build/ARD/core/WMath.cpp.d
@@ -0,0 +1,2 @@
+core/WMath.cpp.o: \
+ /home/angicia/.arduinocdt/packages/arduino/hardware/avr/1.8.3/cores/arduino/WMath.cpp
